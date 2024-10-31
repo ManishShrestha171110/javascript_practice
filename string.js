a4549bf5685e81aef41bfd9da2d24d2f1ca64f9e
Manish Shrestha <manishshrestha1711@gmail.com>
2024-10-31 01:01:21 -0400

    String variable practice

---
+++ string.js
@@ -0,0 +1,10 @@
+var myName="Manish";
+
+var nameLength=myName.length;
+console.log("Length of the given string= ", nameLength);
+
+var upperCase=myName.toUpperCase();
+console.log("My name in upper case= ",upperCase);
+
+var firstThreeLetters=myName.substring(0,3);
+console.log("First three letters of My Name: ",firstThreeLetters);
